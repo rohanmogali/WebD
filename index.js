cb50bac37863ab5ab4baf2f160e1bd1a0183b765
rohanmogali <93003363+rohanmogali@users.noreply.github.com>
2022-10-09 20:00:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomImg = "dice"+randomNumber1+".png";
+var randomImgSrc = "images/"+randomImg;
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImgSrc)
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImg2 = "dice"+randomNumber2+".png";
+var randomImgSrc2 = "images/"+randomImg2;
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImgSrc2)
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins ✔";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML="✔ Player 2 Wins ";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
+
+ 
